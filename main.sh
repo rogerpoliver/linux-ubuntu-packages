8b0551611fadc77fbc2754e06674015f8424390f
Roger Oliveira <rogerpolvr@gmail.com>
2021-07-12 16:05:35 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -40,5 +40,13 @@ echo 'zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions' >> ~/.zshrc
 
+//Get Node Virtual Env
+Wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
+//Node Virtual Env
+echo '\nexport NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc
+
+//Install node lts
+nvm install 14.17.3
 
